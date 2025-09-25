63d5eab7ec32bfb32552d8b2457111f4afaff10f
sswtshoo <56886175+sswtshoo@users.noreply.github.com>
2025-09-25 23:35:22 +0530

    removed old state vars

--- main.go
+++ main.go
@@ -7,21 +7,7 @@ import (
 	"github.com/sswtshoo/pokedexcli/utils/pokecache"
 )
 
-// var nextUrl *string = nil
-// var prevUrl *string = nil
-
 var cache *pokecache.Cache = pokecache.NewCache(time.Millisecond * 5000)
-
-// var currentResponse *pokeapi.ResponseData = nil
-
-// var caughtPokemon = make(map[string]pokeapi.CatchPokemon)
-
-
-
-// var argName *string
-
-
-
 
 func main() {
 	pokeclient := pokeapi.NewClient(5000 * time.Millisecond, 5 * time.Minute)
